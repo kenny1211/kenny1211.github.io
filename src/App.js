2b4bd1cf692809347ff57340dc505d43e5596461
KN <knny.nguyen@gmail.com>
2019-04-28 00:11:28 -0400

    projects.map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Appbar from './components/Appbar';
-import About from './components/About';
+// import About from './components/About';
 import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Appbar />
-        <div className="row justify-content-center">
-          <div className="col-6 m-auto align-items-center" style={{ height: '100%' }}>
+        {/* <div className="row justify-content-center"> */}
+        {/* <div className="col-6 m-auto align-items-center" style={{ height: '100%' }}>
             <About />
-          </div>
-        </div>
+          </div> */}
+        <Portfolio />
+        {/* </div> */}
         <Footer />
       </div>
     );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import projects from '../projects.json';
+import { Card, CardBody, CardTitle, CardLink } from 'reactstrap';
 
 class Portfolio extends Component {
   state = {
     projects
   };
+
+  render() {
+    return (
+      <div className="row">
+        {projects.map(project => {
+          return (
+            <div className="col-12" style={{ alignContent: 'center', marginTop: '5%' }}>
+              <Card
+                key={project.id}
+                style={{ borderColor: 'transparent', background: 'transparent' }}
+              >
+                <span
+                  className={project.icon}
+                  style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}
+                />
+                <CardBody>
+                  <CardLink
+                    style={{ color: 'black', textAlign: 'center' }}
+                    href={project.link}
+                    target="_blank"
+                  >
+                    <CardTitle>{project.name}</CardTitle>
+                  </CardLink>
+                </CardBody>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Portfolio;
